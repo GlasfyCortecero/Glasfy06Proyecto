d6b28bc466dd5ce4f99a108f5970fa475a5c6333
GlasfyCortecero <gmcortecero@misena.edu.co>
2021-06-13 22:03:30 -0500

    Consultando datos de la tabla usuario

---
+++ Glasfy06Proyecto/Controllers/UsuarioController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Glasfy06Proyecto.Models;
+
+namespace Glasfy06Proyecto.Controllers
+{
+    public class UsuarioController : Controller
+    {
+                // GET: Usuario
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return View(db.usuario.ToList());
+            }
+                
+        }
+    }
+}
