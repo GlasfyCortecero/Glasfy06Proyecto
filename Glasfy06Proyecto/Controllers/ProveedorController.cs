171a716e01c0aa36ac4bca3d8ffe73d1041253b7
GlasfyCortecero <gmcortecero@misena.edu.co>
2021-06-20 22:35:59 -0500

    Detalles de los Crud

--- Glasfy06Proyecto/Controllers/ProveedorController.cs
+++ Glasfy06Proyecto/Controllers/ProveedorController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -119,7 +120,8 @@ namespace Glasfy06Proyecto.Controllers
                 return RedirectToAction("Index");
             }
         }
-    }
-         
+        
 
+
+    }
 }
