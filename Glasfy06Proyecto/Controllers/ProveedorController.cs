0e22f4bdcebca8a7355ecfae1f90299110104c4e
GlasfyCortecero <gmcortecero@misena.edu.co>
2021-06-22 21:57:57 -0500

    Cargando Archivos CSV

--- Glasfy06Proyecto/Controllers/ProveedorController.cs
+++ Glasfy06Proyecto/Controllers/ProveedorController.cs
@@ -120,8 +120,63 @@ namespace Glasfy06Proyecto.Controllers
                 return RedirectToAction("Index");
             }
         }
-        
+        public ActionResult uploadCSV()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public ActionResult uploadCSV(HttpPostedFileBase fileForm)
+        {
+            //string para guardar la ruta
+            string filePath = string.Empty;
 
 
+            //condicion para saber si llego el archivo
+            if (fileForm != null)
+            {
+                //ruta de la carpeta que gurdara el archivo
+                string path = Server.MapPath("~/Uploads/");
+
+                //condicion para saber si la ruta de la carpeta existe
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+                //obtener el nombre del archivo
+                filePath = path + Path.GetFileName(fileForm.FileName);
+                //obtener la extension del archivo
+                string extension = Path.GetExtension(fileForm.FileName);
+
+                //guardar el archivo
+                fileForm.SaveAs(filePath);
+
+                string csvData = System.IO.File.ReadAllText(filePath);
+
+                foreach (string row in csvData.Split('\n'))
+                {
+                    if (!string.IsNullOrEmpty(row))
+                    {
+                        var newProveedor = new proveedor
+                        {
+                            nombre = row.Split(';')[0],
+                            direccion = row.Split(';')[1],
+                            telefono = row.Split(';')[2],
+                            nombre_contacto = row.Split(';')[3],
+                        };
+
+                        using (var db = new inventario2021Entities())
+                        {
+                            db.proveedor.Add(newProveedor);
+                            db.SaveChanges();
+                        }
+                    }
+                }
+
+
+            }
+            return View();
+
+        }
     }
 }
